47b2a751036795dfb42660853fe22d240b9c1108
arnaud guibbert <guibberta@gmail.com>
2026-02-03 17:29:59 +0000

    chore: include pre-commit as optional

--- Makefile
+++ Makefile
@@ -3,12 +3,17 @@ TEST_PROJECT_NAME=testpkg
 
 .PHONY: create-tmp-project-no-input
 create-tmp-project-no-input:
+	rm -rf $(TEST_PROJECT_DIR)
 	uv run cookiecutter ./ \
 		--no-input \
 		project_name="$(TEST_PROJECT_NAME)" \
 		project_dir="$(TEST_PROJECT_DIR)" \
-		description="This is a test description."
+		description="This is a test description." \
+		include_gh_cicd=False \
+		include_vscode_settings=False \
+		include_pre_commit=True
 
 .PHONY: create-tmp-project
 create-tmp-project:
+	rm -rf $(TEST_PROJECT_DIR)
 	uv run cookiecutter ./
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,19 @@
+import os
 import shutil
 
 GH_FOLDER = "./.github"
 VSCODE_FOLDER = "./.vscode"
+PRE_COMMIT_CONFIG = "./.pre-commit-config.yaml"
 
 if __name__ == "__main__":
 
     keep_gh_folder = {{ cookiecutter.include_gh_cicd }}
     keep_vscode_folder = {{ cookiecutter.include_vscode_settings }}
+    keep_pre_commit = {{ cookiecutter.include_pre_commit }}
 
     if not keep_gh_folder:
         shutil.rmtree(GH_FOLDER)
     if not keep_vscode_folder:
         shutil.rmtree(VSCODE_FOLDER)
+    if not keep_pre_commit:
+        os.remove(PRE_COMMIT_CONFIG)
